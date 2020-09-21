934e0c07ac9247c28b40eb4ef14cb6cc62b4a2f1
C36-HussainHaidarAlmousawi <70634196+C36-HussainHaidarAlmousawi@users.noreply.github.com>
2020-09-21 03:37:40 +0300

    done

--- ios-cw4-p1.playground/Contents.swift
+++ ios-cw4-p1.playground/Contents.swift
@@ -4,17 +4,19 @@ struct Movie{
     let title , pgRate : String
     let mainCharecters , gnre: [String]
     let imdbRate : Double
-   
+
     func kidsSuitable()-> String {
         if pgRate == "PG" {
         return ("good for kids")
     }else {
     return ("not good for kids")
     }
-   
-    
+
+
 }
+
 }
+
 
 var harryPotter = Movie(title: "harryPotter", pgRate: "PG", mainCharecters: ["Harry" , "Lord" , "Hermione" ], gnre: ["Fantasy" , "Family" , "Adventure"], imdbRate: 7.6)
 
@@ -32,13 +34,26 @@ print(theHobit)
 
 
 class Movie2{
-    var title: String = ""
-    var pgRate: String = ""
-    var mainCharecters: [String] = [""]
-    var gnre: [String] = [""]
-    var imdbRate: Int = 0
-    
-    
+    var title: String
+    var pgRate: String
+    var mainCharecters: [String]
+    var gnre: [String]
+    var imdbRate: Double
+
+
+    init(title: String , mainCharecters: [String] , pgRate: String , gnre: [String] , imdbRate: Int)
+    {
+        self.gnre = gnre
+        self.imdbRate = Double(imdbRate)
+        self.mainCharecters = mainCharecters
+        self.title = title
+        self.pgRate = pgRate
+        
+        
+        
+        
+       
+        
     func kidsSuitable()-> String {
         if pgRate == "PG" {
         return ("good for kids")
@@ -46,8 +61,18 @@ class Movie2{
     return ("not good for kids")
     }
    
-    
+      
 }
+    }
+    func description() {
+        print("the film title is" , title)
+    print("the imdb Rate is" , imdbRate)
+        print("the main charecters is" , mainCharecters)
+        print("the Pg rating is" , pgRate)
+        print("the genre of the film is" , gnre)
+    }
+
 }
 
-
+var frozen = Movie2(title: "frozen", mainCharecters: ["elsa" , "ana"], pgRate: "PG", gnre: ["family", "adventure"], imdbRate: Int(7.6))
+print(frozen)
